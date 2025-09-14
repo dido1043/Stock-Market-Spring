8fccfc7f30c37ec4edfdfd3488b073236245a48b
dido1043 <dudunovski_05@gmail.com>
2025-09-14 21:24:21 +0300

    Init

---
+++ StockMarketSpring/src/main/java/com/example/stockmarketspring/StockMarketSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockmarketspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketSpringApplication.class, args);
+	}
+
+}
---
+++ StockMarketSpring/src/test/java/com/example/stockmarketspring/StockMarketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockmarketspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
