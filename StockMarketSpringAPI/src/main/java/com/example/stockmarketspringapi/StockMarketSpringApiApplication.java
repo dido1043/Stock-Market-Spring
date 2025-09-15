02a59b769d2a5e349cd6fd36130fbcae9859d89d
dido1043 <dudunovski_05@gmail.com>
2025-09-15 11:48:35 +0300

    Reinitialized project

---
+++ StockMarketSpringAPI/src/main/java/com/example/stockmarketspringapi/StockMarketSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockmarketspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMarketSpringApiApplication.class, args);
+    }
+
+}
---
+++ StockMarketSpringAPI/src/test/java/com/example/stockmarketspringapi/StockMarketSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockmarketspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
