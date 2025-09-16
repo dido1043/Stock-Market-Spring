0dddfba650f06ba8f3f7491f0647d51b7847e559
dido1043 <dudunovski_05@gmail.com>
2025-09-16 19:16:24 +0300

    Removed env variables from the tests

--- StockMarketSpringAPI/src/test/java/com/example/stockmarketspringapi/StockMarketSpringApiApplicationTests.java
+++ StockMarketSpringAPI/src/test/java/com/example/stockmarketspringapi/StockMarketSpringApiApplicationTests.java
@@ -1,21 +1,16 @@
 package com.example.stockmarketspringapi;
 
-import com.example.stockmarketspringapi.config.DotenvInitializer;
 import com.example.stockmarketspringapi.controller.CompanyController;
 import com.example.stockmarketspringapi.controller.StockController;
-import com.example.stockmarketspringapi.model.entity.Stock;
 import com.example.stockmarketspringapi.service.interfaces.CompanyService;
 import com.example.stockmarketspringapi.service.interfaces.StockService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Service;
-import org.springframework.test.context.ContextConfiguration;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
-@ContextConfiguration(initializers = DotenvInitializer.class)
 class StockMarketSpringApiApplicationTests {
 
     @Autowired
