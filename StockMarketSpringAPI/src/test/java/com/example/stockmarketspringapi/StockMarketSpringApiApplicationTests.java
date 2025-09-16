ec67a4363f47a50eb91dbc076a6db6edba143988
dido1043 <dudunovski_05@gmail.com>
2025-09-16 18:54:36 +0300

    Added check for applicaton in contextLoads method

--- StockMarketSpringAPI/src/test/java/com/example/stockmarketspringapi/StockMarketSpringApiApplicationTests.java
+++ StockMarketSpringAPI/src/test/java/com/example/stockmarketspringapi/StockMarketSpringApiApplicationTests.java
@@ -1,16 +1,22 @@
-//package com.example.stockmarketspringapi;
-//
-//import com.example.stockmarketspringapi.config.DotenvInitializer;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ContextConfiguration;
-//
-//@SpringBootTest
-//@ContextConfiguration(initializers = DotenvInitializer.class)
-//class StockMarketSpringApiApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.example.stockmarketspringapi;
+
+import com.example.stockmarketspringapi.config.DotenvInitializer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@ContextConfiguration(initializers = DotenvInitializer.class)
+class StockMarketSpringApiApplicationTests {
+
+    @Autowired
+    private StockMarketSpringApiApplication application;
+    @Test
+    void contextLoads() {
+        assertThat(application).isNotNull();
+    }
+
+}
