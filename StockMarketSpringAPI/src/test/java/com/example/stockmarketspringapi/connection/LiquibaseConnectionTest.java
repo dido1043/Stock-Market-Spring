7f8bd49fa17536c2e29a5c44deb093d193a821b2
dido1043 <dudunovski_05@abv.bg>
2025-10-04 18:28:28 +0300

    Add comprehensive test suite with Testcontainers

--- StockMarketSpringAPI/src/test/java/com/example/stockmarketspringapi/connection/LiquibaseConnectionTest.java
+++ StockMarketSpringAPI/src/test/java/com/example/stockmarketspringapi/connection/LiquibaseConnectionTest.java
@@ -1,20 +1,24 @@
 package com.example.stockmarketspringapi.connection;
 
+import com.example.stockmarketspringapi.config.IntegrationTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Import(IntegrationTestConfig.class)
 public class LiquibaseConnectionTest {
 
-  @Autowired JdbcTemplate jdbc;
+    @Autowired
+    JdbcTemplate jdbc;
 
-  @Test
-  void dbConnectionWorks() {
-    Integer one = jdbc.queryForObject("SELECT 1", Integer.class);
-    assertEquals(1, one);
-  }
+    @Test
+    void dbConnectionWorks() {
+        Integer one = jdbc.queryForObject("SELECT 1", Integer.class);
+        assertEquals(1, one);
+    }
 }
