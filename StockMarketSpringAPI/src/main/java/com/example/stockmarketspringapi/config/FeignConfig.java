e9302fcae19e3eb5b718c526cbbfae88c6756bdc
dido1043 <dudunovski_05@abv.bg>
2025-10-17 12:42:50 +0300

    Added feign error decoder

--- StockMarketSpringAPI/src/main/java/com/example/stockmarketspringapi/config/FeignConfig.java
+++ StockMarketSpringAPI/src/main/java/com/example/stockmarketspringapi/config/FeignConfig.java
@@ -1,14 +1,35 @@
 package com.example.stockmarketspringapi.config;
 
+import com.example.stockmarketspringapi.exception.FeignErrorDecoder;
 import feign.Logger;
+import feign.Request;
+import feign.Retryer;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 public class FeignConfig {
+
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public Request.Options requestOptions() {
+        return new Request.Options(10, TimeUnit.SECONDS, 60, TimeUnit.SECONDS, true);
+    }
+
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default(100, TimeUnit.SECONDS.toMillis(1), 3);
+    }
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new FeignErrorDecoder();
     }
 }
