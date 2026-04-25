92d2e25c992af2b2260cc6725751c96230b797b2
dido1043 <dudunovski_05@abv.bg>
2026-04-25 22:29:31 +0300

    Initialize Investment Portfolio Tracker application with Maven setup

---
+++ InvestmentPortfolioTracker/src/main/java/org/ipt/investmentportfoliotracker/InvestmentPortfolioTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.ipt.investmentportfoliotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentPortfolioTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentPortfolioTrackerApplication.class, args);
+    }
+
+}
---
+++ InvestmentPortfolioTracker/src/test/java/org/ipt/investmentportfoliotracker/InvestmentPortfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ipt.investmentportfoliotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentPortfolioTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
